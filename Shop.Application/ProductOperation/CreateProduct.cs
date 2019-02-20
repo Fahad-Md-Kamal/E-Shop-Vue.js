1331803d36f020d8aa41044f8665062f98f3eeed
Big Bug <bigbug@gmail.com>
2019-02-20 22:10:03 +0600

    First Push Of the project

---
+++ Shop.Application/ProductOperation/CreateProduct.cs
@@ -0,0 +1,32 @@
+﻿using Shop.Database;
+using Shop.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shop.Application.ProductOperation
+{
+    public class CreateProduct
+    {
+        private ApplicationDBConetext _context;
+
+        public CreateProduct(ApplicationDBConetext conetext)
+        {
+            _context = conetext;
+        }
+
+        public void Do(int _id, string _name, string _desc, decimal _price)
+        {
+            _context.Product.Add(new Product
+            {
+                Id = _id,
+                Name = _name,
+                Description = _desc,
+                Price = _price
+            });
+        }
+
+
+
+    }
+}
---
+++ Shop.Database/ApplicationDBConetext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Shop.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shop.Database
+{
+    public class ApplicationDBConetext : IdentityDbContext
+    {
+        public ApplicationDBConetext(DbContextOptions options) 
+            : base(options)  {  }
+
+        public DbSet<Product> Product { get; set; }
+    }
+}
